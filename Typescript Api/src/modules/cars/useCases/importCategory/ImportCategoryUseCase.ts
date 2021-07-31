996ccdd695ebfa9584a5868c87d24f10f22872ed
MatheusDantas19 <mind@icomp.ufam.edu.br>
2021-07-31 11:37:25 -0400

    Leitura files csv via stream

--- Typescript Api/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Typescript Api/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,40 @@
+import fs from 'fs'
+import csvParse from 'csv-parse'
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository'
+
+interface IImportCategory {
+    name: string,
+    description: string
+}
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file)
-        console.log("OII")
+
+    constructor(private categoriesRepository: CategoriesRepository){}
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path)
+            const categories: IImportCategory[] = []
+
+            const parseFile = csvParse({
+                delimiter: ';'
+            })
+            stream.pipe(parseFile)
+            parseFile.on("data", async (line) => {
+                const [name, description] = line
+                categories.push({name, description})
+            }).on('end',() => {
+                resolve(categories)
+            }).on('error',(err) => {
+                reject(err)
+            })
+
+        })
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file)
+        console.log(categories)
     }
 
 }
